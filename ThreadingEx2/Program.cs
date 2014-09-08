c64274141273c4eda04bfe9ee3f598530c93b88e
kasper995 <kasper995@hotmail.dk>
2014-09-08 09:30:10 +0200

    begyndt task i generator

--- ThreadingEx2/Program.cs
+++ ThreadingEx2/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace FindSmallest
 {
@@ -36,29 +39,77 @@ namespace FindSmallest
             return smallestSoFar;
         }
 
+        private static int FindBiggest(int[] numbers)
+        {
+            if (numbers.Length < 1)
+            {
+                throw new ArgumentException("There must be at least one element in the array");
+            }
 
-       
+            int biggestSoFar = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > biggestSoFar)
+                {
+                    biggestSoFar = number;
+                }
+            }
+            return biggestSoFar;
+        }
+
+
         static void Main()
         {
-
-           
-            foreach (int[] d in Data)
+            List<Task<int>> myList = new List<Task<int>>();
+          
+            foreach (int[] data in Data)
             {
-                Thread t1 = new Thread(() =>
+
+                Task<int> rTask = Task.Run(() =>
                 {
-                    int smallest = FindSmallest(d);
+                        
+                    int smallest = FindSmallest(data);
 
-                    Console.WriteLine("\t" + String.Join(", ", d) + "\n-> " + smallest);
+                    Console.WriteLine("\t" + String.Join(", ", data) + "\n-> " + smallest);
                     
+                   
+                    return smallest;
                 });
-                t1.Start();
+
+                myList.Add(rTask);
+
+               
+
                 
-
-
-
             }
+
+            Task.WaitAll(myList.ToArray());
+
+
+
+            //foreach (Task<int> t in myList)
+            //{
+            //    Console.Write(t.Result.ToString() + " ");
+                
+            //}
+
+            List<int> rlist = new List<int>();
+
+            foreach (Task<int> t in myList)
+            {
+                rlist.Add(t.Result);
+                
+            }
+
+            int smallestofall = FindSmallest(rlist.ToArray());
+           
+            Console.WriteLine("\t" + String.Join(", ", rlist) + "\n-> " + smallestofall);
+
             
+
             
+            Console.ReadKey();
+           
         }
     }
 }
